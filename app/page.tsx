89a8d941a06214f7a2d7e47bf3780a79169a260e
BosEriko <bos@eriko.ph>
2026-02-27 18:53:14 +0800

    Feature: Use Default Template on Homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
+import Template from "@template";
+
 export default async function Home() {
   return (
-    <div>Under Maintenance</div>
+    <Template.Default>
+      hey
+    </Template.Default>
   );
 }
