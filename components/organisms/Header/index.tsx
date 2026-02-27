49c868d3a99384e96fbdb65d828178c21526d0a1
BosEriko <bos@eriko.ph>
2026-02-27 18:52:51 +0800

    Feature: Update Header

--- components/organisms/Header/index.tsx
+++ components/organisms/Header/index.tsx
@@ -13,17 +13,21 @@ const Header = () => {
   const router = useRouter();
 
   return (
-    <header className="text-black border-b border-gray-200 bg-white">
-      <div className="container mx-auto flex justify-between items-center px-4">
+    <header>
+      <div className="container mx-auto flex justify-between items-center p-4">
         <button onClick={() => router.push("/")}>
           <h2
-            className={`${pixelify.className} text-3xl md:text-4xl font-bold text-[#f7b43d] cursor-pointer`}
+            className={`${pixelify.className} text-3xl md:text-4xl font-bold text-[#F87317] cursor-pointer flex items-center gap-3`}
           >
-            BE
+            <div>🐯</div>
+            <span>TwisWua</span>
           </h2>
         </button>
         <div>
           <Navigation />
+        </div>
+        <div>
+          <a href="https://twitch.tv/TwisWua" target="_blank" className="bg-[#F87317] rounded-full py-2 px-4 text-white font-bold">Go to Twitch</a>
         </div>
       </div>
     </header>
